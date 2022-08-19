e36def1017fdfdd78da4adeb9964eee96c9103ca
ivelinamisova <ivelina_misova@abv.bg>
2022-08-19 14:57:17 +0300

    init

--- turtle_race.py
+++ turtle_race.py
@@ -22,3 +22,23 @@ class racer(object):
         self.tur.penup()
         self.tur.setpos(position)
         self.tur.setheading(90)
+
+    def move_1(self):
+        rand = random.randrange(1, 20)
+        self.position = (self.position[0], self.position[1] + rand)
+        self.tur.penup()
+        self.tur.forward(rand)
+
+    def reset_1(self):
+        self.tur.penup()
+        self.tur.setposition(self.position)
+
+
+def set_up_file(name, colors):
+    file_1 = open(name, 'w')
+    for color in colors:
+        file_1.write(color + '0 \n')
+    file_1.close()
+
+
+def start_game_1():
