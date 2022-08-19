b40516adf39238643ea1aa7a5bd85ac9d190e139
ivelinamisova <ivelina_misova@abv.bg>
2022-08-19 20:19:17 +0300

    init

--- turtle_race.py
+++ turtle_race.py
@@ -1,26 +1,27 @@
 from turtle import *
-from random import randint    # Generating random values
+from random import randint  # Generating random values
 
-speed(0)                      # Give the speed to the turtle and 0 is the fastest speed.
-penup()                       # Move turtle without leaving tracks
-goto(-140, 140)               # Move turtle to the position
+speed(0)  # Give the speed to the turtle and 0 is the fastest speed.
+penup()  # Move turtle without leaving tracks
+goto(-140, 140)  # Move turtle to the position
 
 for step in range(15):
     write(step, align='center')
     right(90)
     for num in range(8):
         penup()
-        forward(10)
-        pendown()
+        forward(10)  # Function is used to move the turtle in the forward direction
+        pendown()  # The default state of turtle. Function is mostly useful to reestablish pendown state
+        # after using .penup()
         forward(10)
     penup()
-    backward(160)
-    left(90)
+    backward(160)  # Function is used to move the turtle in the backward direction
+    left(90)  # Function is used to move the turtle in the left direction
     forward(20)
 
 tur1 = Turtle()
-tur1.color('green')
-tur1.shape('turtle')
+tur1.color('green')  # Give the green color to the turtle
+tur1.shape('turtle')  # Give the shape to the turtle
 
 tur1.penup()
 tur1.goto(-160, 100)
@@ -63,7 +64,7 @@ for turn in range(30):
     tur4.left(12)
 
 for turn in range(100):
-    tur1.forward(randint(1, 5))
+    tur1.forward(randint(1, 5))  # Move forward with a random number of steps
     tur2.forward(randint(1, 5))
     tur3.forward(randint(1, 5))
     tur4.forward(randint(1, 5))
