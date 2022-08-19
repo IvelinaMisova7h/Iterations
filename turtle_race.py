ed6a6f95a5cee9c025b2486ce3d1c3e55db9382f
ivelinamisova <ivelina_misova@abv.bg>
2022-08-19 12:52:28 +0300

    init

--- turtle_race.py
+++ turtle_race.py
@@ -10,4 +10,15 @@ turtles = 8
 
 turtle.screensize(window_width, window_height)
 
+
 class racer(object):
+    # initialize constructor
+    def __init__(self, color, position):
+        self.position = position
+        self.color = color
+        self.tur = turtle.Turtle()
+        self.tur = shape("turtle")
+        self.tur = color(color)
+        self.tur.penup()
+        self.tur.setpos(position)
+        self.tur.setheading(90)
