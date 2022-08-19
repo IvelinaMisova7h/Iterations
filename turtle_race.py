8f646c6485b0cdb3f6a388bcebce37e25cbfdd9d
ivelinamisova <ivelina_misova@abv.bg>
2022-08-19 12:22:48 +0300

    init

---
+++ turtle_race.py
@@ -0,0 +1,15 @@
+import turtle
+
+bob = turtle.Turtle()
+bob.color("red")
+bob.pensize(7)
+bob.shape("turtle")
+
+bob.forward(100)
+bob.left(90)
+bob.penup()
+bob.forward(100)
+bob.right(90)
+bob.pendown()
+bob.color("green")
+bob.forward(100)
